07f9e9f7cdb591c462a26854a95fb15be909ffc8
Christian Sanches dos Santos <chris.sanchees@hotmail.com>
2018-12-09 14:17:41 -0200

    Adicionando Layotus

---
+++ ControleEstoque/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ControleEstoque.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ControleEstoque.Areas.Identity.IdentityHostingStartup))]
+namespace ControleEstoque.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ControleEstoqueContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ControleEstoqueContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<ControleEstoqueContext>();
+            });
+        }
+    }
+}
--- ControleEstoque/Startup.cs
+++ ControleEstoque/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ControleEstoque.Models;
 using ControleEstoque.Models.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -36,6 +38,7 @@ namespace ControleEstoque
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             var connection = @"Server=(localdb)\mssqllocaldb;Database=ControleEstoque;Trusted_Connection=True;ConnectRetryCount=0";
+
             services.AddDbContext<StockContext>
                 (options => options.UseSqlServer(connection));
         }

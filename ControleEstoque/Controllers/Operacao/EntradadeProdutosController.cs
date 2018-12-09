3575d1529120051fd6a788aa0c9eb1598f8e185f
Alisson <alissonroberto25@gmail.com>
2018-12-09 17:07:14 -0200

    Entrada de Produtos view

---
+++ ControleEstoque/Controllers/Operacao/EntradadeProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleEstoque.Controllers
+{
+    public class EntradadeProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
--- ControleEstoque/Models/Entities/Produto.cs
+++ ControleEstoque/Models/Entities/Produto.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Protocols;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
